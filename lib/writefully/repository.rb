e5a30a4aa234c1723b15e0b874a39d5d4d2c069e
Zack Siri <zack@artellectual.com>
2014-04-05 03:24:35 +0700

    pseudo coded worker

--- lib/writefully/repository.rb
+++ lib/writefully/repository.rb
@@ -3,16 +3,26 @@ require 'celluloid'
 
 module Writefully
   class Repository
+    include Celluloid
+
+    attr_reader :owner, :access_token
+
+    def initialize(access_token, owner)
+      @owner = owner
+      @access_token = access_token
+    end
+
     def api
-      @_api ||= ::Github.new
+      @_api ||= ::Github.new oauth_token: access_token
     end
 
-    def create
-      
+    def create blk
+      result = # call github api 
+      blk.call(result)
     end
 
-    def add_hook
-      
+    def add_hook blk
+      result = #
     end
   end
 end
