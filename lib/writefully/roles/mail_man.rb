f37c78f58e3f707c862356989a0068ca76df878d
Zack Siri <zack@artellectual.com>
2014-04-09 22:26:36 +0700

    redis pub / sub now working correctly

--- lib/writefully/roles/mail_man.rb
+++ lib/writefully/roles/mail_man.rb
@@ -3,36 +3,42 @@ module Writefully
     class MailMan
       include Celluloid::IO
 
+      class InvalidMessage < StandardError; end
+
       finalizer :shutdown
+
+      MAILBOX_NAME = 'mailbox:*'
 
       def initialize
         r = ::Redis.new(host: 'localhost', port: 6379, driver: :celluloid)
         @redis = ::Redis::Namespace.new(:"#{Writefully.env}:writefully", redis: r)
-        subscribe
+        async.subscribe
       end
 
       def subscribe
-        @redis.psubscribe('inbox:*') do |on|
-          on.pmessage &async.handle_message
+        @redis.psubscribe(MAILBOX_NAME) do |on|
+          on.pmessage do |match, channel, message|
+            async.handle_message(channel, message)            
+          end
         end 
       end
 
-      def handle_message
-        Proc.new do |match, channel, message|
-          actor_name, action_name = channel.split(':')[1,2]
-          puts actor_name
-          puts action_name
-          # Celluloid::Actor[actor_name].__send__(action_name, message)
-        end
+      def handle_message(channel, message)
+        actor_name, action_name = get_actor_action(channel)
+        # Celluloid::Actor[actor_name].__send__(action_name, message)
       end
 
-      def exit_redis
-        @redis.punsubscribe('inbox:*')
-        @redis.quit
+      def get_actor_action(channel)
+        channel_descriptor = channel.split(':')
+        unless channel_descriptor.count == 5
+          raise InvalidMessage, "Need Actor name and Action name #{MAILBOX_NAME}:actor_name:action_name" 
+        end
+        channel_descriptor[3, 4]
       end
 
       def shutdown
-        async.exit_redis
+        @redis.punsubscribe(MAILBOX_NAME)
+        @redis.disconnect
       end
     end
   end
