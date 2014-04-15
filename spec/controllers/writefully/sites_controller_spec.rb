0e45220e552c59e9c5ecaad0e3b9525a18e2981d
Zack Siri <zack@artellectual.com>
2014-04-15 18:39:52 +0700

    setup test for engine controller

--- spec/controllers/writefully/sites_controller_spec.rb
+++ spec/controllers/writefully/sites_controller_spec.rb
@@ -2,6 +2,23 @@ require 'spec_helper'
 
 module Writefully
   describe SitesController do 
-    
+    routes { Writefully::Engine.routes }
+
+    fixtures :"writefully/sites"
+    fixtures :"writefully/posts"
+    fixtures :"writefully/authorships"
+
+    let(:owner) { writefully_authorships(:wf_owner_1) }
+
+    before do 
+      session[:wf_owner_id] = owner.id
+    end
+
+    describe "#index" do 
+      it "should be success" do 
+        get :index
+        response.should be_success
+      end
+    end
   end
 end
