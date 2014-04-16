b5e5fb70b6377563c305c44c608f02dccef0ccdf
Zack Siri <zack@artellectual.com>
2014-04-16 23:40:58 +0700

    added some sample content

--- lib/writefully/tools/hammer.rb
+++ lib/writefully/tools/hammer.rb
@@ -5,6 +5,10 @@ module Writefully
 
       attr_reader :api, :user_name, :site_name, :domain
 
+      SAMPLE_CONTENT = 
+        { "README"                => "",
+          "meta.yml"              => "",
+          "assets/writefully.png" => "" }
 
       def initialize auth_token, user_name, site_name, domain
         @api = Github.new oauth_token: auth_token
@@ -36,10 +40,19 @@ module Writefully
 
       def add_sample_content repo_name
         Writefully.logger.info "Adding Sample content #{site_name}"
-        apis.repos.contents.create user_name, repo_name
+        # apis.repos.contents.create user_name, repo_name, 
       rescue Exception => e
         raise e
       end
+
+      def sample_content_paths
+        ["1-sample-content/README", "1-sample-content/meta.yml", "1-sample-content/assets/writefully.png"]
+      end
+
+      def sample_content
+        
+      end
+
     end
   end
 end
